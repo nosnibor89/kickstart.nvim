4d1207ee6dc4f52039e97257e562989791d38fb5
Robinson Marquez <nosnibor1989@gmail.com>
2023-08-23 21:55:30 -0400

    Move metals config to plugin file

--- init.lua
+++ init.lua
@@ -537,15 +537,3 @@ cmp.setup {
 -- This is a space for my (Robinson's) custom config: mappings, plugin setups, etc
 vim.keymap.set({ 'n', 'v' }, '<C-p>', require('nvim-tree.api').tree.open, { desc = 'Open File Explorer' })
 vim.keymap.set({ 'n', 'v' }, '<leader>f', vim.lsp.buf.format, { desc = 'Format current buffer with LSP' })
--- Autocmd that will actually be in charging of starting the whole thing
-local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
-  -- NOTE: You may or may not want java included here. You will need it if you
-  -- want basic Java support but it may also conflict if you are using
-  -- something like nvim-jdtls which also works on a java filetype autocmd.
-  pattern = { "scala", "sbt", "java" },
-  callback = function()
-    require("metals").initialize_or_attach({})
-  end,
-  group = nvim_metals_group,
-})
--- lua/custom/plugins/metals.lua
+++ lua/custom/plugins/metals.lua
@@ -1,6 +1,20 @@
 return {
-   'scalameta/nvim-metals',
-    dependencies = {
-	"nvim-lua/plenary.nvim"
-    }
+	'scalameta/nvim-metals',
+	dependencies = {
+		"nvim-lua/plenary.nvim"
+	},
+	config = function()
+		-- Autocmd that will actually be in charging of starting the whole thing
+		local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+		vim.api.nvim_create_autocmd("FileType", {
+			-- NOTE: You may or may not want java included here. You will need it if you
+			-- want basic Java support but it may also conflict if you are using
+			-- something like nvim-jdtls which also works on a java filetype autocmd.
+			pattern = { "scala", "sbt", "java" },
+			callback = function()
+				require("metals").initialize_or_attach({})
+			end,
+			group = nvim_metals_group,
+		})
+	end
 }
