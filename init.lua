3eb55a31498c8dd6c80f2b7b7d37076d4f9d271d
Robinson Lara <rlara@cfchildren.org>
2023-10-03 18:55:59 -0300

    customize gitsign preview hunk

--- init.lua
+++ init.lua
@@ -131,7 +131,8 @@ require('lazy').setup({
         vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
           { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
         vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
-        vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
+        vim.keymap.set('n', '<leader>gsp', require('gitsigns').preview_hunk,
+          { buffer = bufnr, desc = '[P]review [H]unk' })
       end,
     },
   },
--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -3,6 +3,6 @@ return {
 	config = function()
 		vim.g.copilot_node_command = '$HOME/.nvm/versions/node/v16.20.1/bin/node'
 		vim.g.copilot_no_tab_map = true
-		vim.keymap.set("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+		vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', { silent = true, expr = true })
 	end
 }
